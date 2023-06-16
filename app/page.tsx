7dab5a324500fa70c33ee7fdf6aa125b5aeb50da
Pietro Schirano <pietro.schirano@gmail.com>
2023-06-16 11:58:34 +0200

    Differnt UI

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Chat() {
     className="ml-4 bg-blue-500 text-white p-2 rounded"
   >
     {/* Replace this with your plane SVG */}
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-send"><line x1="22" x2="11" y1="2" y2="13"/><polygon points="22 2 15 22 11 13 2 9 22 2"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-send"><line x1="22" x2="11" y1="2" y2="13"/><polygon points="22 2 15 22 11 13 2 9 22 2"/></svg>
   </button>
 </form>
     </div>
